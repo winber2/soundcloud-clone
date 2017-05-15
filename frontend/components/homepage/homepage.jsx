e9106860f73ca8dce4c379cf441c8fefac051bdd
Winber Xu <winber1@gmail.com>
2017-05-15 11:41:54 -0700

    added modal

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
+import Modal from 'react-modal';
 
 class HomePage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { isOpen: false, username: '', password: '' };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.props.signup(user);
+  }
+
+  update(prop) {
+    return e => this.setState({ prop });
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  openModal(e) {
+    e.preventDefault();
+
+    this.setState({ isOpen: true });
+  }
+
+  closeModal(e) {
+    e.preventDefault();
+
+    this.setState({ isOpen: false });
   }
 
   render() {
     return (
       <div className='homepage'>
+        <Modal isOpen={this.state.isOpen} contentLabel="Modal">
+          <h1>Sign in</h1>
+          <input onChange={this.update('username')} placeholder='username'></input>
+          <input onChange={this.update('password')} type='password' placeholder='password'></input>
+          <button onClick={this.handleSubmit}>Sign Up!</button>
+
+          <br />
+          <button onClick={this.closeModal}>Back</button>
+        </Modal>
         <main className='homepage-info'>
           <header>
             <ul>
@@ -15,7 +58,7 @@ class HomePage extends React.Component {
               <p>Logo</p>
             </li>
             <li>
-              <button>Sign in</button>
+              <button onClick={this.openModal}>Sign in</button>
               <button>Create Account</button>
             </li>
             </ul>
---
+++ frontend/components/homepage/signin.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class Signin extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    let user = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.props.signup(user);
+  }
+
+  update(prop) {
+    return e => this.setState({ prop });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Sign in!</h1>
+        <input onChange={this.update('username')} placeholder='username'></input>
+        <input onChange={this.update('password')} type='password' placeholder='password'></input>
+        <button onClick={this.handleSubmit}>Sign Up!</button>
+
+        <br />
+        <button onClick={this.closeModal}>Back</button>
+      </div>
+    );
+  }
+}
