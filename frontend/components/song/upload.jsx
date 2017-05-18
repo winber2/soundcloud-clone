599280520668595791058f509617e67b16f74514
Winber Xu <winber1@gmail.com>
2017-05-18 15:57:04 -0700

    styled upload page

--- frontend/components/song/upload.jsx
+++ frontend/components/song/upload.jsx
@@ -14,26 +14,52 @@ class Upload extends React.Component {
   }
 
   acceptFile(files) {
-    debugger;
+    this.setState({ route: 'input' })
   }
 
   selectRoute() {
-    if (this.state.route === 'upload') {
+    if (this.state.route === '') {
       return (
-        <div className='upload-box'>
-          <ul>
-            <h1>Upload to Vibe</h1>
-          </ul>
-          <ul>
-            <Dropzone className='dropzone' onDrop={this.acceptFile}>
-              <button className='signup'>Choose a file to upload</button>
-            </Dropzone>
-            <button onClick={this.toHome} className='login'>Back to home page</button>
-          </ul>
-        </div>
+        <ul className='dropzone'>
+          <Dropzone className='reset' onDrop={this.acceptFile}>
+            <button className='signup'>Choose a file to upload</button>
+          </Dropzone>
+          <button onClick={this.toHome} className='login'>Back to home page</button>
+        </ul>
       )
     } else {
-
+      return (
+        <ul className='upload-song'>
+          <h1>Song info</h1>
+          <ul className='upload-song-description'>
+            <li className='upload-image-box'>
+              <div id='upload-image-icon' />
+              <button className='upload-image'>Upload Image</button>
+            </li>
+            <ul className='upload-song-info'>
+              Title
+              <input></input>
+              Album <input></input>
+              <ul className='upload-song-detail'>
+                <li>
+                  Genre
+                  <input></input>
+                </li>
+                <li>
+                  Release Date
+                  <input type='date' className='date'></input>
+                </li>
+              </ul>
+              Description
+              <textarea rows='6'></textarea>
+            </ul>
+          </ul>
+          <div className='upload-buttons'>
+            <button className='login'>Cancel</button>
+            <button className='signup'>Save</button>
+          </div>
+        </ul>
+      )
     }
   }
 
@@ -42,7 +68,12 @@ class Upload extends React.Component {
     return (
       <section className='upload-main'>
         <div className='upload-info' />
-          {route}
+          <div className='upload-box active'>
+            <ul>
+              <h1>Upload to Vibe</h1>
+            </ul>
+            {route}
+          </div>
       </section>
     );
   }
