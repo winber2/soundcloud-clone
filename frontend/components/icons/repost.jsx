9f25b7f4beb3ce95b64ca6284e81ca14f58c7b74
Winber Xu <winber1@gmail.com>
2017-06-15 11:01:34 -0700

    repost container

--- frontend/components/icons/repost.jsx
+++ frontend/components/icons/repost.jsx
@@ -3,13 +3,58 @@ import React from 'react';
 class RepostIcon extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { isReposted: false, isActive: '' }
+  }
+
+  findUserFollow(followers, currentUser) {
+    if (followers === undefined) return false;
+    for (let i = 0; i < followers.length; i++) {
+      if (followers[i].id === currentUser.id) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  componentWillReceiveProps(nextProps) {
+    let user = nextProps.user;
+    if (this.findUserFollow(user.followers, nextProps.currentUser)) {
+      this.setState({ isFollowed: true, isActive: 'active' });
+    } else {
+      this.setState({ isFollowed: false, isActive: '' });
+    }
+  }
+
+  componentDidMount() {
+    let user = this.props.user;
+    if (this.findUserFollow(user.followers, this.props.currentUser)) {
+      this.setState({ isFollowed: true, isActive: 'active' });
+    }
+  }
+
+  addRepost() {
+    let currentUser = this.props.currentUser;
+    let user = this.props.user;
+
+    if (this.state.isFollowed === true) {
+      let follow = this.props.user.followers.find( fol => (
+        fol.id === currentUser.id
+      ));
+      this.props.unfollowUser(follow.follow_id)
+        .then(() => this.props.fetchUser(this.props.user.username));
+    } else {
+      this.props.followUser(currentUser, user)
+        .then(() => this.props.fetchUser(this.props.user.username));
+    }
   }
 
   render() {
     return (
       <li className='repost'>
-        <div className='repost-icon'/>
-        <span className='repost'>12</span>
+        <div className={`repost-icon ${this.state.isActive}`}/>
+        <span className={`favorite ${this.state.isActive}`}>
+
+        </span>
       </li>
     );
   }
---
+++ frontend/components/icons/repost_container.js
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { repostSong, deleteRepost } from '../../util/repost_api_util';
+import { fetchSingleSong } from '../../actions/song_actions';
+import Repost from './repost';
+
+const mapStateToProps = (state) => ({
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  repostSong: (currentUser, user) => repostSong(currentUser, user),
+  deleteRepost: id => deleteRepost(id),
+  fetchSingleSong: id => dispatch(fetchSingleSong(id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Repost);
